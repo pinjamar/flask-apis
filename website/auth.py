32c93712abc70ce22ee729b115b806ffd5374468
Ivan <pinjamar@gmail.com>
2023-07-18 10:23:23 +0200

    added flashing and db models

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html", boolean=True)
 
 
@@ -17,4 +15,20 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) > 4:
+            flash('Email must be at least 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be at least 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords dont match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            flash('Account created!', category='success')
     return render_template("sign_up.html")
